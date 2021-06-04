244fbac5df827a32a56140206b0a7dd6d68dfc0c
khannoussi-malek <khannoussimalek@gmail.com>
2021-06-04 01:22:10 +0100

    first step to mobile

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,18 +4,25 @@ import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider, ColorModeScript, CSSReset } from "@chakra-ui/react";
 import App from "./app";
 import theme, { customTheme } from "./theme";
-ReactDOM.render(
-  <React.StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ChakraProvider theme={customTheme}>
-      <CSSReset />
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const startApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <ChakraProvider theme={customTheme}>
+        <CSSReset />
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+  reportWebVitals();
+};
+if (window.cordova) {
+  document.addEventListener("deviceready", startApp, false);
+} else {
+  startApp();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
